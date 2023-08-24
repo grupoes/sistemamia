aaa29f5818f7a5a7250defaee41d093119c99941
70167122 <elvisbravo262@gmail.com>
2023-08-24 09:58:49 -0500

    url de firebase

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -2,7 +2,7 @@
 import cors from 'cors';
 
 const corsOptions = {
-  origin: 'https://us-central1-your-firebase-project.cloudfunctions.net', // Cambia esta URL con la URL correcta de Firebase Functions
+  origin: 'https://console.firebase.google.com/project/whatsapp-api-data/overview', // Cambia esta URL con la URL correcta de Firebase Functions
   methods: 'GET, POST, PUT, DELETE',
   allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
 };
