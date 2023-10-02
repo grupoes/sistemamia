affba7a9e740fad74f533dccaf2619862b9d93d3
70167122 <elvisbravo262@gmail.com>
2023-10-02 18:22:43 -0500

    ssl node

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
+import https from 'https';
+import fs from 'fs';
+
 import app from "./app.js";
+
+import dotenv from "dotenv";
 
 import { sequelize } from "./database/database.js";
 
@@ -21,12 +26,29 @@ import './models/chat.js';
 import './models/estadosConversacion.js';
 import './models/numerosWhatsapp.js';*/
 
+dotenv.config();
+
 async function main() {
     try {
         //await sequelize.sync({alter: true});
 
-        app.listen(4000);
-        console.log('Server listening on port', 4000);
+        if (process.env.ENVIRONMENT === 'DESARROLLO') {
+            app.listen(process.env.PUERTO_APP);
+            console.log('Server listening on port', process.env.PUERTO_APP);
+        } else {
+            const httpsOptions = {
+                key: fs.readFileSync('/etc/sslpay2/private.key'),  // Reemplaza con el path a tu archivo .key
+                cert: fs.readFileSync('/etc/sslpay2/dominio.crt'),   // Reemplaza con el path a tu archivo .crt
+            
+                // Si tienes un certificado intermedio o bundle, lo incluyes así:
+                ca: fs.readFileSync('/etc/sslpay2/bundle')
+            };
+
+            https.createServer(httpsOptions, app).listen(process.env.PUERTO_APP, () => {
+                console.log('Servidor corriendo con HTTPS en https://localhost:4000/');
+            });
+        }
+        
     } catch (error) {
         console.error('Unable to connect to the database:', error);
     }
