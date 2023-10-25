cfca6546d270f2949d2b1e2d16ebc9ede436d50a
70167122 <elvisbravo262@gmail.com>
2023-10-25 09:14:42 -0500

    sdfds

--- src/public/js/chat.js
+++ src/public/js/chat.js
@@ -1851,7 +1851,7 @@ escogePlantilla.addEventListener('change', (e) =>  {
         </div>
         <div class="col-md-12">
             <div class="input-group mb-3">
-                <label for="cuerpo" class="col-form-label">Variables:</label>
+                <label for="cuerpo_" class="col-form-label">Variables:</label>
                 <input type="text" class="form-control" placeholder="Ingrese el contenido de la variable" value="Finaliza octubre" id="contentVariable">
                 <span class="input-group-text" id="basic-addon2">!!_bienvenida_!!</span>
             </div>
