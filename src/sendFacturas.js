744a39d478915d7ea60711848d5e208b6d5d79ed
grupoes <desarrollo.tecnologico.tarapoto@gmail.com>
2025-07-01 08:51:18 -0500

    log

--- src/sendFacturas.js
+++ src/sendFacturas.js
@@ -16,86 +16,124 @@ const logMessage = (message) => {
   console.log(logEntry.trim());
 };
 
-cron.schedule(process.env.CRON_GENERAR_FACTURAS, async () => {
-  try {
-    let conf = {
-      method: "post",
-      maxBodyLength: Infinity,
-      url: process.env.URL_INSERT_HONORARIO,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      data: {
-        ruc: "12345678912",
-      },
-    };
+cron.schedule(
+  process.env.CRON_GENERAR_FACTURAS,
+  async () => {
+    try {
+      let conf = {
+        method: "post",
+        maxBodyLength: Infinity,
+        url: process.env.URL_INSERT_HONORARIO,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: {
+          ruc: "12345678912",
+        },
+      };
 
-    const resp = await axios.request(conf);
-    const hono = resp.data;
+      const resp = await axios.request(conf);
+      const hono = resp.data;
 
-    const idHonorario = hono.registro.id;
+      const idHonorario = hono.registro.id;
 
-    let config = {
-      method: "get",
-      maxBodyLength: Infinity,
-      url: process.env.URL_LISTA_EMPRESAS,
-    };
+      logMessage(`idHonorario: ${idHonorario}`);
 
-    const response = await axios.request(config);
-    const datos = response.data;
+      let config = {
+        method: "get",
+        maxBodyLength: Infinity,
+        url: process.env.URL_LISTA_EMPRESAS,
+      };
 
-    for (const dato of datos) {
-      try {
-        let config2 = {
-          method: "post",
-          maxBodyLength: Infinity,
-          url: process.env.URL_SAVE_FACTURA,
-          headers: {
-            "Content-Type": "application/json",
-          },
-          data: {
-            ruc: dato.ruc,
-            razonSocial: dato.razon_social,
-            productoName: dato.descripcion,
-            precio: dato.monto_mensual,
-          },
-        };
+      const response = await axios.request(config);
+      const datos = response.data;
 
-        const response2 = await axios.request(config2);
-        const dataEnvio = response2.data;
-
-        if (dataEnvio.respuesta === "ok") {
-          let confInsert = {
+      for (const dato of datos) {
+        try {
+          let config2 = {
             method: "post",
             maxBodyLength: Infinity,
-            url: process.env.URL_INSERT_FACTURA,
+            url: process.env.URL_SAVE_FACTURA,
             headers: {
               "Content-Type": "application/json",
             },
             data: {
-              honorario_id: idHonorario,
-              contribuyente_id: dato.id,
-              tipo_doc: dataEnvio.documento.id_tipodoc_electronico,
-              serie_comprobante: dataEnvio.documento.serie_comprobante,
-              numero_comprobante: dataEnvio.documento.numero_comprobante,
-              tipo_envio_sunat: dataEnvio.documento.tipo_envio_sunat,
-              titulo: dataEnvio.titulo,
-              mensaje: dataEnvio.mensaje,
-              url_absoluta_a4: dataEnvio.url_absoluta_a4,
-              url_absoluta_ticket: dataEnvio.url_absoluta_ticket,
-              anio: hono.registro.year,
-              mes: hono.registro.mes,
-              descripcion: hono.registro.descripcion,
-              estado: "aceptado",
-              monto: dato.monto_mensual,
+              ruc: dato.ruc,
+              razonSocial: dato.razon_social,
+              productoName: dato.descripcion,
+              precio: dato.monto_mensual,
             },
           };
 
-          const respInsert = await axios.request(confInsert);
-          const dataInsert = respInsert.data;
+          const response2 = await axios.request(config2);
+          const dataEnvio = response2.data;
 
-          logMessage(`factura insertada: ${dataEnvio.mensaje}`);
-        } else {
+          logMessage(`idHonorario: ${dataEnvio}`);
+
+          if (dataEnvio.respuesta === "ok") {
+            let confInsert = {
+              method: "post",
+              maxBodyLength: Infinity,
+              url: process.env.URL_INSERT_FACTURA,
+              headers: {
+                "Content-Type": "application/json",
+              },
+              data: {
+                honorario_id: idHonorario,
+                contribuyente_id: dato.id,
+                tipo_doc: dataEnvio.documento.id_tipodoc_electronico,
+                serie_comprobante: dataEnvio.documento.serie_comprobante,
+                numero_comprobante: dataEnvio.documento.numero_comprobante,
+                tipo_envio_sunat: dataEnvio.documento.tipo_envio_sunat,
+                titulo: dataEnvio.titulo,
+                mensaje: dataEnvio.mensaje,
+                url_absoluta_a4: dataEnvio.url_absoluta_a4,
+                url_absoluta_ticket: dataEnvio.url_absoluta_ticket,
+                anio: hono.registro.year,
+                mes: hono.registro.mes,
+                descripcion: hono.registro.descripcion,
+                estado: "aceptado",
+                monto: dato.monto_mensual,
+              },
+            };
+
+            const respInsert = await axios.request(confInsert);
+            const dataInsert = respInsert.data;
+
+            logMessage(`factura insertada: ${dataEnvio.mensaje}`);
+          } else {
+            let confInsert = {
+              method: "post",
+              maxBodyLength: Infinity,
+              url: process.env.URL_INSERT_FACTURA,
+              headers: {
+                "Content-Type": "application/json",
+              },
+              data: {
+                honorario_id: idHonorario,
+                contribuyente_id: dato.id,
+                tipo_doc: "",
+                serie_comprobante: "",
+                numero_comprobante: "",
+                tipo_envio_sunat: "",
+                titulo: "",
+                mensaje: "",
+                url_absoluta_a4: "",
+                url_absoluta_ticket: "",
+                anio: hono.registro.year,
+                mes: hono.registro.mes,
+                descripcion: hono.registro.descripcion,
+                estado: "pendiente",
+                monto: dato.monto_mensual,
+              },
+            };
+
+            const respInsert = await axios.request(confInsert);
+            const dataInsert = respInsert.data;
+
+            logMessage(`factura no insertada`);
+          }
+        } catch (error) {
           let confInsert = {
             method: "post",
             maxBodyLength: Infinity,
@@ -125,44 +163,16 @@ cron.schedule(process.env.CRON_GENERAR_FACTURAS, async () => {
           const respInsert = await axios.request(confInsert);
           const dataInsert = respInsert.data;
 
-          logMessage(`factura no insertada`);
+          logMessage(`factura no inserta error`);
         }
-      } catch (error) {
-        let confInsert = {
-          method: "post",
-          maxBodyLength: Infinity,
-          url: process.env.URL_INSERT_FACTURA,
-          headers: {
-            "Content-Type": "application/json",
-          },
-          data: {
-            honorario_id: idHonorario,
-            contribuyente_id: dato.id,
-            tipo_doc: "",
-            serie_comprobante: "",
-            numero_comprobante: "",
-            tipo_envio_sunat: "",
-            titulo: "",
-            mensaje: "",
-            url_absoluta_a4: "",
-            url_absoluta_ticket: "",
-            anio: hono.registro.year,
-            mes: hono.registro.mes,
-            descripcion: hono.registro.descripcion,
-            estado: "pendiente",
-            monto: dato.monto_mensual,
-          },
-        };
-
-        const respInsert = await axios.request(confInsert);
-        const dataInsert = respInsert.data;
-
-        logMessage(`factura no inserta error`);
       }
-    }
-  } catch (error) {
-    console.log(error.message);
+    } catch (error) {
+      console.log(error.message);
 
-    logMessage(`Error al leer las empresas:`);
+      logMessage(`Error al leer las empresas:`);
+    }
+  },
+  {
+    timezone: "America/Lima",
   }
-});
+);
