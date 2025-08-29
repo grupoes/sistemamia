0d71d8f4c6bb0e5e91c452d66cf37d27a003944f
grupoes <desarrollo.tecnologico.tarapoto@gmail.com>
2025-08-29 14:34:57 -0500

    fgdf

--- src/sendNotaVentaServidor.js
+++ src/sendNotaVentaServidor.js
@@ -20,30 +20,7 @@ cron.schedule(
   process.env.CRON_GENERAR_NOTA_VENTA,
   async () => {
     try {
-      const config2 = {
-        method: "post",
-        maxBodyLength: Infinity,
-        url: "http://localhost:8000/api/generar-nota-venta-servidor",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        data: {
-          ruc: "20610266933",
-          razonSocial: "SAN DIEGO GROUP E.I.R.L.",
-          productoName:
-            "SERVICIO POR EL SERVIDOR DEL SISTEMA DE FACTURACION DEL PERIODO: 2025-09-14 AL 2026-09-13",
-          precio: 70.0,
-        },
-      };
-
-      const response2 = await axios.request(config2);
-      const dataEnvio = response2.data;
-
-      console.log(dataEnvio);
-
-      logMessage(`idNotaVenta: ${dataEnvio.respuesta}`);
-
-      /*let conf = {
+      let conf = {
         method: "get",
         maxBodyLength: Infinity,
         url: process.env.URL_GET_CONTRIBUYENTES_SERVIDOR,
@@ -82,7 +59,7 @@ cron.schedule(
             logMessage(`No se pudo generar la nota de venta:`);
           }
         }
-      }*/
+      }
     } catch (error) {
       console.log(error.message);
 
