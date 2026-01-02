1c8aecee6f0fd33aa30daef1f3911fa977c76f3b
grupoes <desarrollo.tecnologico.tarapoto@gmail.com>
2026-01-02 14:28:27 -0500

    boletas de pago

---
+++ src/boletaPago.js
@@ -0,0 +1,79 @@
+import axios from "axios";
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const obtenerBoletasPago = async () => {
+  try {
+    let config = {
+      method: "get",
+      maxBodyLength: Infinity,
+      url: `${process.env.URL_OBTENER_BOLETAS_PAGO}`,
+    };
+    const response = await axios.request(config);
+
+    const boletasPago = response.data;
+
+    for (const boleta of boletasPago) {
+      try {
+        const readApi = await axios.post(
+          process.env.API_READ_BOLETA_PAGO,
+          {
+            pdf_path: `/var/www/html/contabilidad/public/archivos/pdt/${boleta.nameFile}`,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+              // Authorization: `Bearer ${process.env.API_TOKEN}`
+            },
+          }
+        );
+
+        const datosBoleta = readApi.data;
+
+        if (datosBoleta.success == true) {
+          const dataBoleta = datosBoleta.data;
+
+          const saveApi = await axios.post(
+            process.env.API_SAVE_BOLETA_PAGO,
+            {
+              id: boleta.id,
+              fecha_ingreso: dataBoleta.Fecha_ingreso,
+              numero_documento: dataBoleta.NUMERO_DOCUMENTO,
+              tipo_documento: dataBoleta.TIPO_DOCUMENTO,
+              nombres: dataBoleta.nombres,
+              situacion: dataBoleta.situacion,
+              ruc: dataBoleta.RUC,
+            },
+            {
+              headers: {
+                "Content-Type": "application/json",
+                // Authorization: `Bearer ${process.env.API_TOKEN}`
+              },
+            }
+          );
+
+          const responseApi = saveApi.data;
+
+          if (responseApi.status == "success") {
+            console.log(
+              `Boleta de pago ID ${boleta.id} procesada y guardada correctamente.`
+            );
+          } else {
+            console.log(
+              `Error al guardar la boleta de pago ID ${boleta.id}: ${responseApi.message}`
+            );
+          }
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  } catch (error) {
+    console.log(error.message);
+    throw new Error("Error al obtener la boleta de pago");
+  }
+};
+
+obtenerBoletasPago();
