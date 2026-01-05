d1099b6fd8f49060b7d4710956d651532fdec0ec
grupoes <desarrollo.tecnologico.tarapoto@gmail.com>
2026-01-05 18:19:01 -0500

    enviar notas

--- src/sendNotaVentaServidor.js
+++ src/sendNotaVentaServidor.js
@@ -23,11 +23,11 @@ cron.schedule(
       let conf = {
         method: "get",
         maxBodyLength: Infinity,
-        url: process.env.URL_GET_CONTRIBUYENTES_SERVIDOR,
+        url: process.env.URL_GET_CONTRIBUYENTES_SERVIDOR_AHORA,
       };
 
       const resp = await axios.request(conf);
-      const data_servidor = resp.data;
+      const data_servidor = resp.data.data;
 
       for (const dato of data_servidor) {
         if (dato.pagos === "ok") {
@@ -50,9 +50,25 @@ cron.schedule(
             const response2 = await axios.request(config2);
             const dataEnvio = response2.data;
 
-            console.log(dataEnvio);
+            if (dataEnvio.respuesta === "ok") {
+              const config3 = {
+                method: "post",
+                maxBodyLength: Infinity,
+                url: process.env.UPDATE_GENERAR_NOTA_VENTA,
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                data: {
+                  id: dato.idpago,
+                  numero_nota: dataEnvio.documento.numero_comprobante,
+                  url_pdf: dataEnvio.url_absoluta_a4,
+                },
+              };
 
-            logMessage(`idNotaVenta: ${dataEnvio.respuesta}`);
+              const response3 = await axios.request(config3);
+
+              console.log(response3.data);
+            }
           } catch (error) {
             console.log(error.message);
 
